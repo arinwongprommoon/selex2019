6ce128054d196d120fec14e01b162b85dd8645e8
arinwongprommoon <bht9216@gmail.com>
2019-03-20 16:30:23 +0000

    ANALYSIS: put correlation at the end

--- analysis/kmer_alt.R
+++ analysis/kmer_alt.R
@@ -12,8 +12,8 @@ ycnt <- readRDS("~/selex2019/kmer_cnt_R_EMSASELEX/Trulig147v1III-Arin1-NuEMSA-Ph
 
 # Define axis labels here
 # (trust me, this is easier than extracting this info from the filenames)
-xlabel = "CpGmC_bound fold change"
-ylabel = "CpGmC_unbound fold change"
+xlabel = "CpGmC bound fold change"
+ylabel = "CpGmC unbound fold change"
 
 kmers <- nrow(normcnt)
 
@@ -26,17 +26,6 @@ colnames(ycnt_norm) <- c("kmer", "y_normcounts")
 # Prepare data frame for plotting
 mydata <- data.frame(normcnt$kmer, xcnt_norm$x_normcounts, ycnt_norm$y_normcounts, kmerbasefreqs)
 colnames(mydata) <- c("kmer", "x_normcounts", "y_normcounts", "no_a", "no_c", "no_g", "no_t")
-
-# rr <- cor.test(mydata$x_normcounts, mydata$y_normcounts, method="pearson", conf.level=0.95)
-
-## Stuff about correlation
-# Function to calculate correlation
-# corr_eqn <- function(x,y, digits = 3) {
-#     corr_coef <- round(cor(x, y), digits = digits)
-#     paste("R = ", corr_coef)
-# }
-# # Put labels and coordinates into data frame to enhance resolution
-# rrlabel = corr_eqn(mydata$x_normcounts, mydata$y_normcounts)
 
 # Extend colour palettes to cover 10 values (0-9)
 NewGreens <- colorRampPalette(brewer.pal(9,"Greens"))(10)
@@ -127,3 +116,6 @@ plot_a <- ggplot(mydata, aes(x=x_normcounts, y=y_normcounts)) +
 
 # Putting them all together
 grid.arrange(plot_a, plot_c, plot_g, plot_t, nrow = 2)
+
+# Spit out correlation
+cor.test(mydata$x_normcounts, mydata$y_normcounts, method="pearson", conf.level=0.95)
